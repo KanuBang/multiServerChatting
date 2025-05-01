317e6d96a7e7cdf659ec56d2b65c16e90ed9c374
gdrffg <gdrffg@gmail.com>
2025-05-01 17:10:46 +0900

    initial setting

---
+++ src/main/java/org/example/multiserverchattingdemo/MultiServerChattingDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.multiserverchattingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiServerChattingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiServerChattingDemoApplication.class, args);
+    }
+
+}
